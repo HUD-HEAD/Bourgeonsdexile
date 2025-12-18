6612dc238ff1b50f43c451e4ec033873fc136f37
kler <claire.delhomme.2942@student.uu.se>
2025-12-18 12:14:17 +0100

    basic transition trigger at the end of animation/timer for passive panels

--- components/utils/comics/panel_transition_trigger.gd
+++ components/utils/comics/panel_transition_trigger.gd
@@ -1,1 +1,21 @@
+##Used on "passive" comic panels to trigger transition when animation is finished, or at the end of timer
 extends Node
+
+##Transition will trigger when animation ends
+@export var anim_player : AnimationPlayer
+##If no animation player is given, will wait this long before triggering transition
+@export var timer_wait : float = 5.0
+
+func _ready() -> void:
+	#Trigger at end of animation if present
+	if anim_player != null :
+		anim_player.animation_finished.connect(SignalManager.next_panel.emit.unbind(1))
+	#Otherwise create a timer and wait for timeout
+	else :
+		var timer : Timer = Timer.new()
+		timer.wait_time = timer_wait
+		timer.one_shot = true
+		timer.timeout.connect(SignalManager.next_panel.emit)
+		add_child(timer)
+		timer.start()
+		
