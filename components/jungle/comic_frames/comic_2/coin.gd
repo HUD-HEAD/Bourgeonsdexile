22334af89deff9ee55bd6deb98088c652a6f9507
kler <claire.delhomme.2942@student.uu.se>
2025-12-29 18:43:42 +0100

    basic comic 2

---
+++ components/jungle/comic_frames/comic_2/coin.gd
@@ -0,0 +1,10 @@
+extends Clickable
+
+## Amount coin fills in progress bar, between 0 and 100
+@export var value : float = 5.0
+@export var progress_bar : TextureProgressBar
+
+func _action_on_click():
+	progress_bar.value += value
+
+	super()
