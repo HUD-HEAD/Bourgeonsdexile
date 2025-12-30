ec6f945011d00928a4a80e5220860eedd3fac41d
kler <claire.delhomme.2942@student.uu.se>
2025-12-30 08:44:41 +0100

    click to start argument panel

---
+++ components/utils/click_enable.gd
@@ -0,0 +1,10 @@
+extends Clickable
+
+@export var node_to_enable : Node
+
+func _ready() -> void:
+	node_to_enable.process_mode = Node.PROCESS_MODE_DISABLED
+	super()
+
+func _action_on_click():
+	node_to_enable.process_mode = Node.PROCESS_MODE_INHERIT
