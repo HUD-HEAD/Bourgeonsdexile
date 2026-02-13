d35c4686845b07fc4baabd2c6b30567a98ba9cbb
kler <48253901+Kleeer@users.noreply.github.com>
2026-02-13 11:03:12 +0100

    Update area_reveal.gd

--- components/utils/area_reveal.gd
+++ components/utils/area_reveal.gd
@@ -9,4 +9,4 @@ func _ready() -> void:
 	
 
 func _on_area_entered(area2d : Area2D):
-	node_to_reveal.create_tween().tween_property(node_to_reveal, "self_modulate.a", 1.0, 3.0)
+	node_to_reveal.create_tween().tween_property(node_to_reveal, "self_modulate", Color.WHITE, 3.0)
