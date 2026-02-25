56761116c4c0f8dd956f1f69efd3315e39b58bb4
kler <48253901+Kleeer@users.noreply.github.com>
2026-02-26 00:57:17 +0100

    LB when inside buildings, hide outside

--- components/utils/area_reveal.gd
+++ components/utils/area_reveal.gd
@@ -1,12 +1,27 @@
 extends Area2D
 
 @export var node_to_reveal : Node2D
+@export var background_mask : CanvasItem
 
 func _ready() -> void:
-	node_to_reveal.modulate.a = 0.0
-	node_to_reveal.show()
-	area_entered.connect(_on_area_entered)
+	_init_visual(node_to_reveal)
+	_init_visual(background_mask)
 	
+	area_entered.connect(_on_area_entered)
+	area_exited.connect(_on_area_exited)
+
+func _init_visual(node : CanvasItem):
+	node.modulate.a = 0.0
+	node.show()
 
 func _on_area_entered(area2d : Area2D):
-	node_to_reveal.create_tween().tween_property(node_to_reveal, "modulate", Color.WHITE, 3.0)
+	_transition(node_to_reveal, Color.WHITE)
+	_transition(background_mask, Color.WHITE)
+
+func _on_area_exited(area2d : Area2D):
+	_transition(node_to_reveal, Color.TRANSPARENT)
+	_transition(background_mask, Color.TRANSPARENT)
+
+func _transition(node : CanvasItem, color : Color):
+	node.create_tween().tween_property(node, "modulate", color, 1.0)
+	
