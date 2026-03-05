a50c43dd50bc69ed7cbc76f66544082e07f53700
kler <48253901+Kleeer@users.noreply.github.com>
2026-03-05 09:18:11 +0100

    implement sound candle, soup, breathing, click arrow (on interactable)

--- components/globals/audio_manager.gd
+++ components/globals/audio_manager.gd
@@ -3,6 +3,7 @@ extends Node
 
 var audio_ambient : AudioStreamPlayer
 var audio_sfx : AudioStreamPlayer
+var audio_sfx_click : AudioStreamPlayer
 
 
 func _ready() -> void:
@@ -11,3 +12,13 @@ func _ready() -> void:
 	
 	audio_sfx = AudioStreamPlayer.new()
 	add_child(audio_sfx)
+	audio_sfx.bus = "Sfx"
+	
+	audio_sfx_click = AudioStreamPlayer.new()
+	add_child(audio_sfx_click)
+	audio_sfx_click.bus = "Sfx"
+	audio_sfx_click.stream = load("res://audio/chapter1/sfx/CLICK_SELECTION_ARROW.wav")
+
+
+func play_click():
+	audio_sfx_click.play()
