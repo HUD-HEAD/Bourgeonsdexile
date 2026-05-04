cf82b8802d32f8fcbd0503025e2bb33e6908cb54
kler <48253901+Kleeer@users.noreply.github.com>
2026-05-04 10:12:32 +0200

    fix companion

--- components/chapter1/main_characters/companion.gd
+++ components/chapter1/main_characters/companion.gd
@@ -1,5 +1,8 @@
 extends Woman
+#TODO refactor/cleanup 
+
 @export var model : Woman
+@export var offset_from_model : float = 0
 
 func _ready() -> void:
 	model.char_area.area_entered.connect(_stop_walking.unbind(1))
@@ -8,3 +11,14 @@ func _ready() -> void:
 	
 	if path_follow != null:
 		walk_along_path = true
+		offset_from_model = self.path_follow.progress - model.path_follow.progress
+	else:
+		offset_from_model = self.global_position.x - model.global_position.x
+
+func _process(delta: float) -> void:
+	anim_sprite.animation = model.anim_sprite.animation
+	if model.walking :
+		if walk_along_path:
+			path_follow.progress = model.path_follow.progress + offset_from_model
+		else:
+			global_position.x = model.global_position.x + offset_from_model
