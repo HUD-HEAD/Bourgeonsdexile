1d2b94cf32224a92ad53689d88915356c3f319f0
kler <48253901+Kleeer@users.noreply.github.com>
2026-05-08 13:20:24 +0200

    get rid of pseudo-modulate in contraction wobble shader to use built-in modulate

--- components/chapter2/contractions/contraction_wobble/contraction_controller.gd
+++ components/chapter2/contractions/contraction_wobble/contraction_controller.gd
@@ -3,6 +3,8 @@ extends Node
 @export var shader_mat : ShaderMaterial
 @export var shader_params : Array[ShaderParam]
 
+@export var contraction_overlay : CanvasItem
+
 func _ready() -> void:
 	for sp in shader_params:
 		shader_mat.set_shader_parameter(sp.name, sp.curr_val)
@@ -17,3 +19,17 @@ func soothe_contraction(delta):
 			var new_val = move_toward(sp.curr_val, sp.min_val, delta)
 			shader_mat.set_shader_parameter(sp.name, new_val)
 			sp.curr_val = new_val
+
+func trigger_contraction():
+	for sp in shader_params:
+		shader_mat.set_shader_parameter(sp.name, sp.max_val)
+		sp.curr_val = sp.max_val
+	
+	var tween_modulate : Tween = self.create_tween()
+	tween_modulate.tween_property(contraction_overlay, "modulate:a", 1.0, 3.0)
+	
+func stop_contraction():
+	var tween_modulate : Tween = self.create_tween()
+	tween_modulate.tween_property(contraction_overlay, "modulate:a", 0.0, 3.0)
+
+	
