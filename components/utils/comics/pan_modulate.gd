23d057f3cb6355c2173c13983bc4b1187cc79f55
kler <48253901+Kleeer@users.noreply.github.com>
2026-05-11 17:07:48 +0200

    fix panel 12 going from night to day

--- components/utils/comics/pan_modulate.gd
+++ components/utils/comics/pan_modulate.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @export var sensitivity : float = 1.0
+@export var invert : bool = false
 
 #HACK
 @export var node_to_enable : Node
@@ -9,7 +10,7 @@ extends Node2D
 #TODO optimize cursor shape signals
 
 func _ready() -> void:
-	self_modulate.a = 0.0
+	self_modulate.a = 0.0 if !invert else 1.0
 	
 	node_to_enable.process_mode = Node.PROCESS_MODE_DISABLED
 
@@ -18,9 +19,12 @@ func _unhandled_input(event: InputEvent) -> void:
 		SignalManager.set_cursor_shape.emit(Input.CURSOR_MOVE)
 		#Panning to the right only
 		if event.relative.x < 0:
-			self_modulate.a = clampf( self_modulate.a - event.relative.x*sensitivity,0,1)
+			var added_val = event.relative.x*sensitivity
+			if invert :
+				added_val = -added_val
+			self_modulate.a = clampf( self_modulate.a - added_val,0,1)
 
-			if self_modulate.a >= 1.0:
+			if (!invert && self_modulate.a >= 1.0) or (invert && self_modulate.a <= 0.0):
 				complete()
 				
 	else:
