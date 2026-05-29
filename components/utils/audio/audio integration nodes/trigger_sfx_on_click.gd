068cf549235f8b86e7a886d0e1db81b030290a72
Adrihc13 <adr13suki@gmail.com>
2026-05-29 13:50:23 +0200

    Integrate Audio Chapter1 comic & level 3

--- components/utils/audio/audio integration nodes/trigger_sfx_on_click.gd
+++ components/utils/audio/audio integration nodes/trigger_sfx_on_click.gd
@@ -2,10 +2,14 @@ class_name TriggerSfxOnClick
 extends Node
 
 @export var clickables : Array[Clickable]
-@export var use_audio_stream: bool = false
 @export var sfx: AudioConfiguration.sfx_type
-@export var stream: AudioStream
 @export var play_sfx_once: bool = false
+@export var use_audio_stream: bool = false
+@export_group("Stream Config")
+@export var stream: AudioStream
+@export var linear_volume: float = 1
+@export var min_pitch: float = 1
+@export var max_pitch: float = 1
 
 func _ready() -> void:
 	for clickable in clickables:
@@ -13,7 +17,7 @@ func _ready() -> void:
 
 func play_sfx():
 	if use_audio_stream:
-		AudioManager.play_audio_stream(stream)
+		AudioManager.play_audio_stream(stream, linear_volume, min_pitch, max_pitch)
 	else:
 		AudioManager.play_sfx(sfx)
 	
