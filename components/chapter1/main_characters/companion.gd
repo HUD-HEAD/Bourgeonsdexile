5e240f6f8ce958623d01adeeda4e335791f82024
kler <48253901+Kleeer@users.noreply.github.com>
2026-06-02 13:35:14 +0200

    lajas blancas queue reworked

--- components/chapter1/main_characters/companion.gd
+++ components/chapter1/main_characters/companion.gd
@@ -3,17 +3,25 @@ class_name Companion extends Woman
 
 @export var model : Woman
 @export var offset_from_model : float = 0
+@export var start_walking : bool = true
 
 func _ready() -> void:
 	model.char_area.area_entered.connect(_stop_walking.unbind(1))
 	SignalManager.obstacle_cleared.connect(_start_walking)
-	_start_walking()
+	if start_walking:
+		_start_walking()
+	else :
+		_stop_walking()
 	
 	if path_follow != null:
 		walk_along_path = true
 		offset_from_model = self.path_follow.progress - model.path_follow.progress
 	else:
 		offset_from_model = self.global_position.x - model.global_position.x
+
+func _start_walking():
+	if can_process():
+		super()
 
 func disable():
 	hide()
