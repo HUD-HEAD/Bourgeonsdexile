398b39eacf61f2403cf9d3d438b6c5b33ac6336e
Adrihc13 <adr13suki@gmail.com>
2026-06-04 17:12:49 +0200

    WIP - Scene Transition

---
+++ components/utils/comics/chapter_ender.gd
@@ -0,0 +1,9 @@
+class_name ChapterEnder
+extends Node
+
+@onready var transition_scene: PackedScene = preload("res://scenes/menus/scene_transition.tscn")
+var transition_scene_instance: CanvasLayer
+
+func trigger_scene_transition():
+	transition_scene_instance = transition_scene.instantiate()
+	GameManager.add_child(transition_scene_instance)
