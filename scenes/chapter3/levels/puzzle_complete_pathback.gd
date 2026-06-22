2e90b10ed2700f43c604effcfe765e0dc062b38a
kler <48253901+Kleeer@users.noreply.github.com>
2026-06-22 10:57:04 +0200

    cleanup

--- scenes/chapter3/levels/puzzle_complete_pathback.gd
+++ scenes/chapter3/levels/puzzle_complete_pathback.gd
@@ -18,14 +18,17 @@ func _ready() -> void:
 func _on_puzzle_complete():
 	wayto.hide()
 	wayback.show()
-	
+	_go_back()
+
+## Transition to path back if defined, or turn around on current path
+func _go_back():
 	if pathfollow_back != null :
+		#Make sure character is not offset from new path
 		var path_back : Path2D = pathfollow_back.get_parent()
 		var new_progress = path_back.curve.get_closest_offset(woman.global_position)
 		pathfollow_back.progress = new_progress
+		#Switch to path back
 		woman.path_follow = pathfollow_back
-		woman.reparent(pathfollow_back, true)
-		
-		
+		woman.reparent(pathfollow_back, true)	
 	else :
 		woman.dir *= -1
