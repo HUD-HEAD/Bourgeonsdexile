7ecbc2d0250718917f26d9ae14d05e31f6541847
kler <48253901+Kleeer@users.noreply.github.com>
2026-06-22 10:51:57 +0200

    Fix offset from path on path back

--- scenes/chapter3/levels/puzzle_complete_pathback.gd
+++ scenes/chapter3/levels/puzzle_complete_pathback.gd
@@ -20,7 +20,12 @@ func _on_puzzle_complete():
 	wayback.show()
 	
 	if pathfollow_back != null :
-		woman.reparent(pathfollow_back, true)
+		var path_back : Path2D = pathfollow_back.get_parent()
+		var new_progress = path_back.curve.get_closest_offset(woman.global_position)
+		pathfollow_back.progress = new_progress
 		woman.path_follow = pathfollow_back
+		woman.reparent(pathfollow_back, true)
+		
+		
 	else :
 		woman.dir *= -1
