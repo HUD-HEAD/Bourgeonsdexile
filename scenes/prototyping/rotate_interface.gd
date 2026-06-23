6db143f5ccc78f7cb31e315e95d80328c1b830ed
kler <48253901+Kleeer@users.noreply.github.com>
2026-06-23 14:48:26 +0200

    update rotate interface

--- scenes/prototyping/rotate_interface.gd
+++ scenes/prototyping/rotate_interface.gd
@@ -3,11 +3,9 @@ class_name RotationInterface
 extends Node2D
 
 @export var turn_clockwise : BaseButton
-@export var turn_counterclockwise : BaseButton
 
 func _ready() -> void:
 	global_rotation_degrees = 0
 
 func connect_piece(piece : PuzzlePieceRotatable):
 	turn_clockwise.pressed.connect(piece.rotate_custom.bind(true))
-	turn_counterclockwise.pressed.connect(piece.rotate_custom.bind(false))
