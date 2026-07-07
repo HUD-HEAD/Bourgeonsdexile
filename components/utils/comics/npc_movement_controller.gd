2d1dbf4274ca610f5f8c605aeceae67ef9d94cc1
Adrihc13 <adr13suki@gmail.com>
2026-07-07 14:07:39 +0200

    integrate walking npc on chapter 1 Jungle

--- components/utils/comics/npc_movement_controller.gd
+++ components/utils/comics/npc_movement_controller.gd
@@ -12,6 +12,11 @@ extends VisibleOnScreenNotifier2D
 @export var pre_puzzle_distance: float
 @export var post_puzzle_distance: float
 @export var movement_speed : float = 10
+
+@export_group("scale_parameters")
+@export var change_scale:bool = false
+@export var idle_scale: float
+@export var walking_scale: float
 var next_distance: float
 
 var is_moving: bool 
@@ -19,9 +24,12 @@ var is_moving: bool
 const idle_key: String = "idle"
 const walking_key: String = "walking"
 
+#only let the npc to stop once
+var can_stop: bool
 
 func _ready() -> void:
 	is_moving = false
+	can_stop = true
 	if start_walking:
 		npc.position.x += pre_puzzle_distance
 	
@@ -36,6 +44,7 @@ func _process(delta: float) -> void:
 		npc.position.x += movement_speed * delta
 		next_distance -= movement_speed * delta
 		if next_distance <= 0:
+			is_moving = false
 			_stop_movement(null)
 
 func _start_movement():
@@ -43,14 +52,24 @@ func _start_movement():
 		anim_sprite.play(walking_key)
 		is_moving = true
 		next_distance = abs(pre_puzzle_distance)
+		
+		if change_scale:
+			anim_sprite.scale = Vector2.ONE * walking_scale
 	else:
 		_stop_movement(null)
 
 func _stop_movement(_area2d : Area2D):
-	is_moving = false
-	anim_sprite.play(idle_key)
+	if can_stop:
+		is_moving = false
+		anim_sprite.play(idle_key)
+		if change_scale:
+			anim_sprite.scale = Vector2.ONE * idle_scale
 
 func _continue_movement():
 	anim_sprite.play(walking_key)
 	is_moving = true
+	can_stop = false
 	next_distance = abs(post_puzzle_distance)
+	
+	if change_scale:
+		anim_sprite.scale = Vector2.ONE * walking_scale
