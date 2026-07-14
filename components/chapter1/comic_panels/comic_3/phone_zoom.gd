96dc4c060f0b69db795bfd5491491574364e09a3
Adrihc13 <adr13suki@gmail.com>
2026-07-14 13:56:05 +0200

    add custom min & max scale for map_phone panels and Polish jungle phone

--- components/chapter1/comic_panels/comic_3/phone_zoom.gd
+++ components/chapter1/comic_panels/comic_3/phone_zoom.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 @export var zoom_in : Clickable
 @export var zoom_out : Clickable
+@export var min_map_scale: float = 1
+@export var max_map_scale: float = 3
 
 var step : float = 0.5
 
@@ -10,9 +12,9 @@ func _ready() -> void:
 	zoom_out.clicked.connect(_on_zoom_out)
 	
 func _on_zoom_in():
-	var new_scale = clampf(scale.x + step, 1.0, 3.0)
+	var new_scale = clampf(scale.x + step, min_map_scale, max_map_scale)
 	self.scale = Vector2(new_scale, new_scale)
 
 func _on_zoom_out():
-	var new_scale = clampf(scale.x - step, 1.0, 3.0)
+	var new_scale = clampf(scale.x - step, min_map_scale, max_map_scale)
 	self.scale = Vector2(new_scale, new_scale)
