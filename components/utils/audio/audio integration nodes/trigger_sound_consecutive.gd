a6a55d425b3d39cd7fb2066b9eca44aac31235ed
Adrihc13 <adr13suki@gmail.com>
2026-07-15 12:17:31 +0200

    Fix multiple baby sounds after pausing the game a few minutes and increase volume of voices speaking

--- components/utils/audio/audio integration nodes/trigger_sound_consecutive.gd
+++ components/utils/audio/audio integration nodes/trigger_sound_consecutive.gd
@@ -3,6 +3,7 @@ extends VisibleOnScreenNotifier2D
 
 
 @export var delay_time: float = 10
+var timer: float = 0
 
 @export var sfx_list: Array[AudioStream]
 @export var linear_volume: float = 1
@@ -10,15 +11,33 @@ extends VisibleOnScreenNotifier2D
 @export var max_pitch: float = 1
 
 var current_scene : Node = null
-
+var start_playing_sfx: bool = false
 
 func _ready() -> void:
 	current_scene = SceneManager.current_scene
-	self.screen_entered.connect(_triger_sound)
+	#self.screen_entered.connect(_triger_sound)
+	self.screen_entered.connect(_start_sfx)
 
 
-func _triger_sound():
-	if current_scene == SceneManager.current_scene:
-		AudioManager.play_audio_stream(sfx_list[randi_range(0,sfx_list.size()-1)], linear_volume, min_pitch, max_pitch, false)
-		await get_tree().create_timer(delay_time).timeout
-		_triger_sound()
+func _start_sfx():
+	start_playing_sfx = true
+	timer = delay_time
+	self.screen_entered.disconnect(_start_sfx)
+
+# Timer didnt stop when the game was paused, now we calculate the timer manually in process func
+#func _triger_sound():
+	#if current_scene == SceneManager.current_scene:
+		#AudioManager.play_audio_stream(sfx_list[randi_range(0,sfx_list.size()-1)], linear_volume, min_pitch, max_pitch, false)
+		#await get_tree().create_timer(delay_time, true).timeout
+		#_triger_sound()
+
+
+func _process(delta: float) -> void:
+	if start_playing_sfx:
+		if current_scene == SceneManager.current_scene:
+			timer += delta
+			if timer >= delay_time:
+				timer = 0
+				AudioManager.play_audio_stream(sfx_list[randi_range(0,sfx_list.size()-1)], linear_volume, min_pitch, max_pitch, false)
+		else:
+			start_playing_sfx = false
